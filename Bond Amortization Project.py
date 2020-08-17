b460de70299a7d985c4de30f1d8aa3d402c912a9
Hao <52360303+hc-solver@users.noreply.github.com>
2020-08-16 21:54:56 -0400

    Update Bond Amortization Project.py

--- Bond Amortization Project.py
+++ Bond Amortization Project.py
@@ -10,25 +10,12 @@ x = 0
 c = 0 #PV of the discount payment  
 PV_Par = par/(1+mr)
 Yr = int(input("At the end of which Year:"))
-if Yr > Life:
+while Yr > Life:
     print ("Error: The year you entered is over maturity period:"+str(Life))
-    Yr = input(input("Please re-enter:"))
-    while n in range(m-Yr+1):     #tocalculate the PV of the interest payment 
+    Yr = int(input("Please re-enter:"))
+while n in range(m-Yr+1):    
         cp = cp/(1+mr)
-        PV_Par = PV_Par/(1+mr)  #to calculate the PV of the Par value
-        x = x + cp
-        n += 1
-    PV_Par = PV_Par*(1+mr)
-    Lol = PV_Par + x
-    print ("Current Value of the last period:"+str(Lol))
-    print ("Interest Expense:"+str(Lol*mr))
-    print ("Coupon Paid:"+str(par*coupon_rate))
-    print ("Premium Amortization:"+str(Lol*mr-par*coupon_rate))
-    
-else:
-    while n in range(m-Yr+1):     #tocalculate the PV of the interest payment 
-        cp = cp/(1+mr)
-        PV_Par = PV_Par/(1+mr)  #to calculate the PV of the Par value
+        PV_Par = PV_Par/(1+mr)  
         x = x + cp
         n += 1
 PV_Par = PV_Par*(1+mr)
@@ -37,6 +24,28 @@ print ("Current Value of the last period:"+str(Lol))
 print ("Interest Expense:"+str(Lol*mr))
 print ("Coupon Paid:"+str(par*coupon_rate))
 print ("Premium Amortization:"+str(Lol*mr-par*coupon_rate))
+Ask = input("Would you like to continue?(Y/N)")
+while Ask == "Y":
+    Yr = int(input("At the end of which Year:"))
+    while Yr > Life:
+        print ("Error: The year you entered is over maturity period:"+str(Life))
+        Yr = int(input("Please re-enter:"))
+    cp = float(par*coupon_rate)
+    PV_Par = par/(1+mr)
+    x = 0
+    n = 0
+    while n in range(m-Yr+1):     
+        cp = cp/(1+mr)
+        PV_Par = PV_Par/(1+mr)  
+        x = x + cp
+        n += 1
+    PV_Par = PV_Par*(1+mr)
+    Lol = PV_Par + x
+    print ("Current Value of the last period:"+str(Lol))
+    print ("Interest Expense:"+str(Lol*mr))
+    print ("Coupon Paid:"+str(par*coupon_rate))
+    print ("Premium Amortization:"+str(Lol*mr-par*coupon_rate))
+    Ask = input("Would you like to continue?(Y/N)")
 
 
     
